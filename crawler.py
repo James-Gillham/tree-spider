96326d53a8c9472186d2e16e4f2a24e620a14256
James-Gillham <jwgillham98@gmail.com>
2017-03-14 20:36:22 +1030

    small updates

--- crawler.py
+++ crawler.py
@@ -20,7 +20,8 @@ def tree_spider(max_pages, product):
             title = link.string
             href = 'https://www.gumtree.com.au' + link.parent.get('href')
 
-            print(title.lstrip())
+            print(title.lstrip().rstrip())
+
             get_ad_data(href)
 
         page += 1
@@ -38,11 +39,25 @@ def get_ad_data(ad_url):
         for ad_price in found_price:
             print(str(ad_price.string).lstrip())
 
+def check_entries(search, pages):
+    #Poorly planned query validation
+
+    search = str(search)
+    search.strip("\'")
+    if not search.isalnum():
+        search = ""
+
+    if not pages.isnumeric():
+        pages = 1
+
+    tree_spider(int(pages), search)
+
+
 def main():
     search = input("What would you like to search for on Gumtree?\n")
     pages = input("How many pages would you like to crawl?\n")
 
-    tree_spider(int(pages), search)
+    check_entries(search, pages)
 
 if __name__ == '__main__':
     main()
